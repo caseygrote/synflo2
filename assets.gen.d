372c92c390adba960ecccc176337282891398d92
caseygrote <caseygrote@gmail.com>
2015-01-15 23:32:43 -0500

    bare bones sensor code taken from sifteo examples

---
+++ assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,121 @@
+main.o: main.cpp \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/audio.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/bluetooth.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/cube.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/event.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/filesystem.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/metadata.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/motion.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/string.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/system.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/time.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/usb.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/color.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/sprite.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/framebuffer.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0rom.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg1.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg2.h \
+  C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/audio.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/bluetooth.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/cube.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/event.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/filesystem.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/metadata.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/motion.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/string.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/system.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/time.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/usb.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/color.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/sprite.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/framebuffer.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0rom.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg1.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg2.h:
+
+C:\Users\cgrote\Desktop\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/tilebuffer.h:
+
+assets.gen.h:
